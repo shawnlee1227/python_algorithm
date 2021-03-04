7380bdaa0d28c51bafcec72a49d88789d23dfd85
shawnlee1227 <shawn.lee1227@gmail.com>
2021-03-03 20:39:22 -0500

    stack class

---
+++ stack.py
@@ -0,0 +1,31 @@
+class Stack:
+
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop(self):
+        return self.items.pop()
+    
+    def is_empty(self):
+        return self.items == []
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+    
+    def get_stack(self):
+        return self.items
+
+
+myStack = Stack()
+myStack.push("CEA")
+myStack.push("PLB")
+print(myStack.get_stack())
+myStack.push("ZVM")
+print(myStack.get_stack())
+myStack.pop()
+print(myStack.get_stack())
+print(myStack.peek())
