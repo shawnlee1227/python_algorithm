0f40c3e0257071f646c583adb63a4e67399277f0
shawnlee1227 <shawn.lee1227@gmail.com>
2021-03-20 11:01:08 -0400

    re-try

--- stack.py
+++ stack.py
@@ -1,3 +1,6 @@
+"""
+Stack Data Structure
+"""
 class Stack:
 
     def __init__(self):
@@ -8,25 +11,26 @@ class Stack:
     
     def pop(self):
         return self.items.pop()
-    
+
     def is_empty(self):
         return self.items == []
-    
+
     def peek(self):
-        if not self.is_empty():
+        if not self.is_empty(): 
             return self.items[-1]
-    
+
     def get_stack(self):
         return self.items
 
 
 if __name__ == "__main__":
-    myStack = Stack()
-    myStack.push("CEA")
-    myStack.push("PLB")
-    print(myStack.get_stack())
-    myStack.push("ZVM")
-    print(myStack.get_stack())
-    myStack.pop()
-    print(myStack.get_stack())
-    print(myStack.peek())
+    s = Stack()
+    print(s.is_empty())
+    s.push('A')
+    s.push('B')
+    print(s.get_stack())
+    s.push('C')
+    print(s.get_stack())
+    s.pop()
+    print(s.get_stack())
+    print(s.peek())
